1a1736e5278c0c3707066bbe00b54c63408a8c1a
shamila13 <149713170+shamila13@users.noreply.github.com>
2024-04-23 13:17:15 +0530

    Update App.java

--- SnakeLadder/App.java
+++ SnakeLadder/App.java
@@ -1,5 +1,7 @@
+import board.Board;
 import coordinates.Coordinates;
 import dice.Dice;
+import game.Game;
 import java.util.*;
 import jumper.Jumper;
 import player.Player;
@@ -7,6 +9,13 @@ import player.Player;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+        HashMap<String, Jumper> map = new HashMap<>();
+        map.put("6", new Jumper(new Coordinates(9,5), new Coordinates(6,6)));
+        map.put("97", new Jumper(new Coordinates(0,1), new Coordinates(9,9)));
+        map.put("21", new Jumper(new Coordinates(7,0), new Coordinates(0,9)));
+        map.put("64", new Jumper(new Coordinates(3,3), new Coordinates(7,4)));
+        
         Player p1 = new Player();
         p1.setPlayerDetailsFromUserInput(p1);
         Player p2 = new Player();
@@ -14,5 +23,12 @@ public class App {
 
         System.out.println("Player name: " + p1.getPlayerName());
         System.out.println("Player name: " + p2.getPlayerName());
+
+        Board b = new Board(10, map);
+        b.printBoard();
+
+        Game game = new Game(b, new Player[] {p1,p2}, new Dice());
+        game.play();
     }
+    
 }
